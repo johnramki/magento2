5166d742a483e0ce2a649fb8c10f379e7582d9b7
Ramki <ramki@Admins-MacBook-Air.local>
2022-03-21 12:16:24 +0530

    Magento2 Category attribute MT-3

---
+++ Category/Block/Category/View.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Codem\Category\Block\Category;
+
+
+class View extends \Magento\Catalog\Block\Category\View
+{    
+    public  function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Catalog\Model\Layer\Resolver $layerResolver,
+        \Magento\Framework\Registry $registry,
+        \Magento\Catalog\Helper\Category $categoryHelper,
+        array $data = [])
+    {
+        parent::__construct($context, $layerResolver, $registry, $categoryHelper, $data);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCmsBlockBottomHtml()
+    {
+        
+        return $this->getLayout()
+        ->createBlock(\Magento\Cms\Block\Block::class)
+        ->setBlockId($this->getCurrentCategory()->getData('futured_category_block_list')) //replace my_cmsblock_identifier with real CMS bock identifier
+        ->toHtml();
+    }
+}
