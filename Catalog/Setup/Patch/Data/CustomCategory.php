a4d649f123e554ee45f21faab5169a1de895e0bd
Ramki <30864805+johnramki@users.noreply.github.com>
2022-03-30 13:37:57 +0530

    Add files via upload
    Redirect

---
+++ Catalog/Setup/Patch/Data/CustomCategory.php
@@ -0,0 +1,82 @@
+<?php
+declare (strict_types = 1);
+
+namespace Codem\Catalog\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+/**
+ * Class CreateCustomAttr for Create Custom Product Attribute using Data Patch.
+ */
+class CustomCategory implements DataPatchInterface {
+
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory          $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply() {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        //Attribute for product (Redirect Category)
+        $eavSetup->addAttribute('catalog_product', 'redirect_category', [
+                'type' => 'int',
+                'label' => 'Redirect Category',
+                'input' => 'select',
+                'source' => 'Codem\Catalog\Model\Config\Source\ProductAttributes',
+                'global' => \Magento\Catalog\Model\ResourceModel\Eav\Attribute::SCOPE_STORE,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => null,
+                'group' => 'General Information',
+                'backend' => ''
+        ]);
+
+       
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies() {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases() {
+        return [];
+    }
+}
+
